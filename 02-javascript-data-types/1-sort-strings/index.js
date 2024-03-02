4fb7f294747455ab94381d6ba7da07b5b74f354d
thonngo <amineva.mira@yandex.ru>
2024-03-02 11:53:25 +0300

    done 1 task module 2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    let sortedArray=arr.slice(arr);
+    if(param === 'asc') {
+        sortedArray.sort((a,b)=> a.localeCompare(b, 'kf', {caseFirst: 'upper'})) 
+    }
+    if(param === 'desc') {
+       sortedArray.sort((a,b) => b.localeCompare(a))
+    }
+    return sortedArray;
 }

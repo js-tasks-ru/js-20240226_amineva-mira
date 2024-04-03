2b8523ad80096160713b0ac2790f5e2437925d31
thonngo <amineva.mira@yandex.ru>
2024-04-03 23:22:25 +0300

    done Tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,54 @@
 class Tooltip {
-  initialize () {
+  static instance;
 
+  constructor() {
+      if (Tooltip.instance) {
+          return Tooltip.instance;
+      }      
+      Tooltip.instance = this;
+  }
+
+  createElement(template = "") {
+    const element = document.createElement('div');
+    element.classList.add('tooltip');
+    element.innerHTML = template;
+    return element
+  }
+
+  render(template) {
+    this.element = this.createElement(template);
+    document.body.append(this.element);
+  }
+
+  initialize () {
+    this.tooltipListners();
+  }
+
+  tooltipListners() {
+    document.addEventListener('pointerover', (event) => this.handelDocumentPointerOver(event));
+    document.addEventListener('pointerout', (event) => this.handelDocumentPointerOut(event));
+  }
+
+  handelDocumentPointerOver(event) {
+    if(event.target.dataset.tooltip) {
+      this.render(event.target.dataset.tooltip)
+      this.element.style.top = event.pageY + 'px'
+      this.element.style.left = event.pageX + 'px' 
+    }
+  }
+
+  handelDocumentPointerOut(event) {
+    this.destroy()
+  }
+
+  destroy() {
+    this.remove()
+  }
+
+  remove() {
+    this.element.remove();
+    document.removeEventListener('pointerover', (event) => this.handelDocumentPointerOver(event));
+    document.removeEventListener('pointerout', (event) => this.handelDocumentPointerOut(event));
   }
 }
 

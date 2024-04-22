e9611d7a0073dfd0f2da26f16217e6a86b7eb358
thonngo <amineva.mira@yandex.ru>
2024-04-22 14:11:54 +0300

    problem 1 with ColumnChart

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -4,7 +4,8 @@ import ColumnChart1 from '../../04-oop-basic-intro-to-dom/1-column-chart/index.j
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ColumnChart extends ColumnChart1 {
-  
+    // from;
+    // to
     constructor(options = {}) {
         super(options)
         this.from = options.range?.from
@@ -13,10 +14,14 @@ export default class ColumnChart extends ColumnChart1 {
     }
 
     async update() {
+        // console.log('from and to in update', from, to)
+        // console.log('thisfrom and thisto in update', this.from, this.to)
+        console.log('1')
         try{
+            console.log('try')
         const data = await this.fetchData(this.from, this.to)
         
-        // console.log('data', data)
+        console.log('data', data)
         super.update(data)
         }
         catch(error){console.error(error);}
@@ -24,11 +29,14 @@ export default class ColumnChart extends ColumnChart1 {
     }
 
      async fetchData() {
+        console.log('2')
+        // console.log('from and to in fetchData', from, to)
+        // console.log('thisfrom and thisto in fetchData', this.from, this.to)
         const newData = new URL(this.url, BACKEND_URL);
 
         newData.searchParams.set('from', this.from);
         newData.searchParams.set('to', this.to)
-        // console.log(newData)
+        // console.log('newDate', newData)
         return await fetchJson(newData)
     }
 
